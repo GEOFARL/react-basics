d772e99965bc457687642cfc40a47813e69ca41c
Maxim <toper.one111@gmail.com>
2023-03-04 23:01:59 +0200

    Add CSS styling to a components
    - Created a css file which is named the same
      as a component and located in the same folder
    - Imported css file into the component file
    - Added classes using a className in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css';
+
 export default function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 4th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
       </div>
-      <div>$294.67</div>
     </div>
   );
 }
