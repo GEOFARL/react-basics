ef93303f135ba7cbb7a10996351719b344a2d34d
Maxim <toper.one111@gmail.com>
2023-03-04 22:45:18 +0200

    Elaborate my component
    - Added more than one html element inside
      a component
    - Found out that I should use only one top
      level html element when returning,
      cannot have sibling elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,11 @@
 export default function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>March 4th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>$294.67</div>
+    </div>
+  );
 }
