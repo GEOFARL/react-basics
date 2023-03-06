9da4f9c9edda79c3a6653f7b21f93cd39d8bed03
Maxim <toper.one111@gmail.com>
2023-03-06 21:29:28 +0200

    Add ExpenseForm component to gather user data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -24,18 +25,18 @@ const App = () => {
     },
   ];
 
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses expenses={expenses} />
-  //   </div>
-  // );
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h2', {}, "Let's get started!"),
-    React.createElement(Expenses, { expenses: expenses })
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
   );
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { expenses: expenses })
+  // );
 };
 
 export default App;
